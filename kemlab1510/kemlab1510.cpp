136164a2d6784658a84d172d7435fbf801d826fd
dmssj <letrey19@gmail.com>
2024-10-19 18:05:19 +0700

    рассчет суммы выполнен в функции

--- kemlab1510/kemlab1510.cpp
+++ kemlab1510/kemlab1510.cpp
@@ -1,11 +1,12 @@
 #include <iostream>     //использование процедур и функций
 
-int main() {
-    setlocale(LC_ALL, "RU");
-    int D[] = { 7, 12, 5, 17, 3 };
-    int sum = 0, sum_кв = 0, sum_куб = 0;
 
-    for (int i = 0; i < 5; i++) {
+void sums(int D[], int s) {
+    int sum = 0;
+    int sum_кв = 0;
+    int sum_куб = 0;
+
+    for (int i = 0; i < s; i++) {
         sum += D[i];
         sum_кв += D[i] * D[i];
         sum_куб += D[i] * D[i] * D[i];
@@ -14,6 +15,14 @@ int main() {
     std::cout << "сумма элементов  " << sum << std::endl;
     std::cout << "сумма квадратов  " << sum_кв << std::endl;
     std::cout << "сумма кубов  " << sum_куб << std::endl;
+}
 
+int main() {
+    setlocale(LC_ALL, "RU");
+    int D[] = { 7, 12, 5, 17, 3 };
+    int s = 5;
+
+    sums(D, s);
+    
     return 0;
 }
